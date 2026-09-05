c77eabc310136d98dc87834839d23598134308cd
monomonomononon-code <monomonomononon@gmail.com>
2026-09-06 00:37:11 +0900

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -481,7 +481,7 @@
         roomsLoaded = true;
       }
       if (!honmaru3dLoaded) {
-        honmaru3dSub.iframe.src = "pages/honmaru3d.html?v=season-spring-6";
+        honmaru3dSub.iframe.src = "pages/honmaru3d.html?v=season-all-1";
         honmaru3dLoaded = true;
       } else {
         sendRoomsToHonmaru3d();
