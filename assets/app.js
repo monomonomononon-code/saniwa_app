14e90eaf49454b79faf4ecf0f8a6e23adcc001c5
monomonomononon-code <monomonomononon@gmail.com>
2026-09-08 01:06:08 +0900

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -466,7 +466,7 @@
       roomsMenuLoaded = true;
     }
     if (id === "journal-view" && !journalLoaded) {
-      journalSub.iframe.src = "pages/journal.html?v=fixed-storage-note-1";
+      journalSub.iframe.src = "pages/journal.html?v=caret-follow-2";
       journalLoaded = true;
     }
     if (id === "backup-view") {
